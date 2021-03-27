8603b72889f7b292fd9a535f0510fa3cf387e196
kenneth cartel <kcartel@svengroup.com>
2021-03-27 20:57:08 +0800

    change connection

--- admin/connection.php
+++ admin/connection.php
@@ -1,22 +1,9 @@
 <?php
 
-	// $servername = "localhost";
-	// $username = "root";
-	// $password = "";
-	// $database = "db_eguide";
-
-	// // Create connection
-	// $conn = mysqli_connect($servername, $username, $password, $database);
-
-	// // Check connection
-	// if (!$conn) {
-	//     die("Connection failed: " . mysqli_connect_error());
-	// }
-
 	$servername = "localhost";
-	$username = "u187511933_eguide";
-	$password = "!Creatives2020";
-	$database = "u187511933_eguide";
+	$username = "root";
+	$password = "";
+	$database = "db_eguide";
 
 	// Create connection
 	$conn = mysqli_connect($servername, $username, $password, $database);
@@ -24,6 +11,19 @@
 	// Check connection
 	if (!$conn) {
 	    die("Connection failed: " . mysqli_connect_error());
-    } 
+	}
+
+	// $servername = "localhost";
+	// $username = "u187511933_eguide";
+	// $password = "!Creatives2020";
+	// $database = "u187511933_eguide";
+
+	// // Create connection
+	// $conn = mysqli_connect($servername, $username, $password, $database);
+
+	// // Check connection
+	// if (!$conn) {
+	//     die("Connection failed: " . mysqli_connect_error());
+    // } 
 
 ?> 
