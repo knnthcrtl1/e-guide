b8478eea7d7d99dd4859741867b78fbeda6e3321
kenneth cartel <kcartel@svengroup.com>
2021-05-09 01:13:35 +0800

    send notif

--- admin/custom-js/notification-script.js
+++ admin/custom-js/notification-script.js
@@ -40,11 +40,12 @@ $(document).ready(function () {
             url: "./functions/function-student.php",
             data: studentFormData + "&ajax=true",
             success: function (data) {
+                alert(data);
                 // if(data == 1){
                 //     alert('email already exists, please use other email');
                 //     return false;
                 // }
-                alert("Added Successfully!");
+                // alert("Added Successfully!");
             }
         });
     });
--- admin/functions/function-notification.php
+++ admin/functions/function-notification.php
@@ -12,15 +12,25 @@ if (isset($_POST['ajax'])) {
         $studentId = mysqli_real_escape_string($conn, (strip_tags($_POST['notification-student-id'])));
         $notificationTitle = mysqli_real_escape_string($conn, (strip_tags($_POST['notification-title'])));
         $notificationMessage = mysqli_real_escape_string($conn, (strip_tags($_POST['notification-message'])));
-        
+
         $studentTableFields = "notification_student,notification_title,notification_message";
 
-        $sql = "INSERT INTO tbl_notifications ( {$studentTableFields} ) VALUES 
-                ('{$studentId}','{$notificationTitle}','{$notificationMessage}')";
+        // $sql = "SELECT * FROM tbl_students WHERE student_id = '{$studentId}'";
+        // $result = mysqli_query($conn, $sql);
+        // $row = mysqli_fetch_array($result);
+   
+        $to = "vortexbears@gmail.com";
+        $subject = $notificationTitle;
+        $txt = $notificationMessage;
+        $headers = "From: test@email.com";
 
-        if (!mysqli_query($conn, $sql)) {
-            echo ("Error description: " . mysqli_error($conn));
-        }
+        mail($to, $subject, $txt, $headers);
 
+        // $sql = "INSERT INTO tbl_notifications ( {$studentTableFields} ) VALUES 
+        //         ('{$studentId}','{$notificationTitle}','{$notificationMessage}')";
+
+        // if (!mysqli_query($conn, $sql)) {
+        //     echo ("Error description: " . mysqli_error($conn));
+        // }
     }
 }
