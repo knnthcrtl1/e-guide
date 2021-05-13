e7018843800d23690cb9f57162cd4a8eb8384e26
kenneth cartel <kcartel@svengroup.com>
2021-05-13 11:35:21 +0800

    update student

--- view_notification.php
+++ view_notification.php
@@ -3,59 +3,57 @@
 
 <body>
   <div class="wrapper ">
-    <?php
-    include('sidebar.php');
-    navigationList('notification');
-    ?>
-    <div class="main-panel">
-      <!-- Navbar -->
-      <?php
-      include('navbar.php');
-      $headerTitle = 'Notification';
-      navbarContainer($headerTitle);
-      ?>
-      <!-- End Navbar -->
-      <div class="content">
-        <div class="container-fluid">
+    <div class="content">
+      <div class="container">
+        <div class="row">
+          <div class="logo__user__container">
+            <img src="./admin/assets/img/logo.png" />
+          </div>
+        </div>
+        <hr />
+        <?php include('./new_menus.php');
+        newMenus();
+        ?>
 
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="card">
-                <div class="card-header card-header-primary">
-                  <h4 class="card-title ">Simple Table</h4>
-                  <p class="card-category"> Here is a subtitle for this table</p>
-                </div>
-                <div class="card-body">
-                  <div class="table-responsive" id="notificationTable">
-                    <table class="table" id="studentNotificationTable">
-                      <thead class="text-primary">
-                        <tr>
-                          <th>
-                            ID
-                          </th>
-                          <th>
-                            Title
-                          </th>
-                          <th>
-                            Message
-                          </th>
-                          <th>
-                            Date
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="card">
+              <div class="card-header card-header-primary">
+                <h4 class="card-title ">Simple Table</h4>
+                <p class="card-category"> Here is a subtitle for this table</p>
+              </div>
+              <div class="card-body">
+                <div class="table-responsive" id="notificationTable">
+                  <table class="table" id="studentNotificationTable">
+                    <thead class="text-primary">
+                      <tr>
+                        <th>
+                          ID
+                        </th>
+                        <th>
+                          Title
+                        </th>
+                        <th>
+                          Message
+                        </th>
+                        <th>
+                          Date
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
 
-                      </tbody>
-                    </table>
-                  </div>
+                    </tbody>
+                  </table>
                 </div>
               </div>
             </div>
+          </div>
 
         </div>
         <!-- your content here -->
       </div>
     </div>
-    <?php include('footer.php'); ?>
-    <script src="./js/notification-script.js"></script>
+  </div>
+  <?php include('footer.php'); ?>
+  <script src="./js/notification-script.js"></script>
