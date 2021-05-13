28a4f14dedfe3f09b0702bdbc8f8a08e1e2e543d
kenneth cartel <kcartel@svengroup.com>
2021-05-13 11:35:21 +0800

    update student

---
+++ new_menus.php
@@ -0,0 +1,54 @@
+<?php
+
+
+
+function newMenus()
+{
+
+?>
+    <div class="row">
+        <div class="col-md-4 ">
+            <a href="./view_student.php" class="dashboard__fillup__container--link">
+                <div class="dashboard__fillup__container">
+                    FILL-UP FORM
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <div class="dashboard__fillup__container">
+                ABOUT
+            </div>
+        </div>
+        <div class="col-md-4">
+            <a href="./view_notification.php" class="dashboard__fillup__container--link">
+                <div class="dashboard__fillup__container">
+                    NOTIFICATION
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <a href="#" class="dashboard__fillup__container--link">
+                <div class="dashboard__fillup__container">
+                    SEND REQUEST
+                </div>
+            </a>
+        </div>
+        <div class="col-md-4">
+            <div class="dashboard__fillup__container">
+                test
+            </div>
+        </div>
+        <div class="col-md-4">
+            <a href="./logout.php" class="dashboard__fillup__container--link">
+                <div class="dashboard__fillup__container">
+                    LOGOUT
+                </div>
+            </a>
+        </div>
+    </div>
+<?php
+}
+
+?>
