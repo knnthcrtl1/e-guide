689b35e7b24f66d4cabc2d98575eb6ae23dc208a
kenneth cartel <kcartel@svengroup.com>
2021-05-14 18:46:14 +0800

    update student spiel

--- new_menus.php
+++ new_menus.php
@@ -28,26 +28,14 @@ function newMenus()
         </div>
     </div>
     <div class="row">
-        <div class="col-md-3">
-            <a href="#" class="dashboard__fillup__container--link">
-                <div class="dashboard__fillup__container">
-                    SEND REQUEST
-                </div>
-            </a>
-        </div>
-        <div class="col-md-3">
-            <div class="dashboard__fillup__container">
-                test
-            </div>
-        </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <a href="./change_password.php" class="dashboard__fillup__container--link">
                 <div class="dashboard__fillup__container">
                     CHANGE PASSWORD
                 </div>
             </a>
         </div>
-        <div class="col-md-3">
+        <div class="col-md-4">
             <a href="./logout.php" class="dashboard__fillup__container--link">
                 <div class="dashboard__fillup__container">
                     LOGOUT
