5d602b9cabee07a07cd30fb6266bc95bee82a7dd
kenneth cartel <kcartel@svengroup.com>
2021-05-15 19:06:27 +0800

    fix backgrounds

--- change_password.php
+++ change_password.php
@@ -5,12 +5,6 @@
 
         <div class="content">
             <div class="container">
-                <div class="row">
-                    <div class="logo__user__container">
-                        <img src="./assets/img/logo.png" />
-                    </div>
-                </div>
-                <hr />
                 <?php include('./new_menus.php');
                 newMenus();
                 ?>
