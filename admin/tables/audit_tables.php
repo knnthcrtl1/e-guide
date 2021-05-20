407acfa621393cb5caff1d950efbf44bd7b0c5d8
kenneth cartel <kcartel@svengroup.com>
2021-05-20 23:21:35 +0800

    update student number in login

--- admin/tables/audit_tables.php
+++ admin/tables/audit_tables.php
@@ -15,7 +15,7 @@ if (mysqli_num_rows($result) != 0) {
             <td><?php echo $row['audit_id']; ?></td>
             <?php
 
-            $sql2 = "SELECT * FROM tbl_students WHERE student_id = '{$row['audit_username']}'";
+            $sql2 = "SELECT * FROM tbl_students WHERE student_stud_id = '{$row['audit_username']}'";
             $result2 = mysqli_query($conn, $sql2);
             $row2 = mysqli_fetch_array($result2);
 
@@ -23,10 +23,10 @@ if (mysqli_num_rows($result) != 0) {
             ?>
             <td><?php echo $studentName; ?></td>
             <td><?php
-                if ($row['student_type'] == 0) echo "Grade School";
-                if ($row['student_type'] == 1) echo "High School";
-                if ($row['student_type'] == 2) echo "Senior High School";
-                if ($row['student_type'] == 3) echo "College";
+                if ($row2['student_type'] == 0) echo "Grade School";
+                if ($row2['student_type'] == 1) echo "High School";
+                if ($row2['student_type'] == 2) echo "Senior High School";
+                if ($row2['student_type'] == 3) echo "College";
                 ?>
             </td>
             <td><?php echo $row['audit_username']; ?></td>
