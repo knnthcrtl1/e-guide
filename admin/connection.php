e9898864521904462aa11535ba30c689602f7080
kenneth cartel <kcartel@svengroup.com>
2021-05-24 13:38:23 +0800

    add connection

--- admin/connection.php
+++ admin/connection.php
@@ -1,23 +1,23 @@
 <?php
 
-// $servername = "localhost";
-// $username = "root";
-// $password = "";
-// $database = "db_eguide";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "db_eguide";
 
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password, $database);
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-// // Check connection
-// if (!$conn) {
-// 	die("Connection failed: " . mysqli_connect_error());
-// }
+// Check connection
+if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+}
 
-// return false;
+return false;
 
 	$servername = "localhost";
 	$username = "u187511933_eguide";
-	$password = "!Creatives2020";
+	$password = "!Creatives2022";
 	$database = "u187511933_eguide";
 
 	// Create connection
