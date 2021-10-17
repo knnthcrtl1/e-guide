abf66cc50aeb942e5cf5e728c780cafe87f8c441
knnthcrtl1 <knnthcrtl.kc@gmail.com>
2021-10-17 19:30:18 +0800

    add max width

--- admin/view_dashboard.php
+++ admin/view_dashboard.php
@@ -1,15 +1,15 @@
 <?php include('./header.php'); ?>
 
 <body class="" style="background-image: url(./assets/img/student-dashboard.jpg); color: #ffffff; background-repeat: no-repeat;">
-  <div class="wrapper " >
+  <div class="wrapper ">
 
     <div class="content">
-      <div class="container">
+      <div class="container" style='max-width: 720px;'>
         <div class="row">
           <div class="logo__user__container">
             <img src="./assets/img/logo.png" />
           </div>
-      </div>
+        </div>
         <hr />
         <?php include('./newMenus.php');
         newMenus();
