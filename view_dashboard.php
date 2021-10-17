c65b9241c49b59c5aa1caa568b852bcd250f0aa2
knnthcrtl1 <knnthcrtl.kc@gmail.com>
2021-10-17 11:37:16 +0800

    update dashboard

--- view_dashboard.php
+++ view_dashboard.php
@@ -1,6 +1,6 @@
 <?php include('./header.php'); ?>
 
-<body class="" style="background-image: url(./admin/assets/img/student-dashboard.jpg); color: #ffffff; background-repeat: no-repeat; background-color: rgba(0,0,0,0.😎">
+<body class="" style="background-image: url(./admin/assets/img/student-dashboard.jpg); color: #ffffff; background-repeat: no-repeat;">
   <div class="wrapper " style="background-color: rgba(0,0,0,0.8)">
 
     <div class="content">
