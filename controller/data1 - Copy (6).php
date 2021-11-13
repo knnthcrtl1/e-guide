6299ee750ab9b71cd2e712172f1d06ee587e74ac
Shidny Tugade <shidnyariel@gmail.com>
2021-11-13 17:06:05 +0800

    testing hotfix

---
+++ controller/data1 - Copy (6).php
@@ -0,0 +1,123 @@
+<?php include('./header.php'); ?>
+
+<body  style="background-image: url(./admin/assets/img/student-dashboard.jpg); color: #ffffff;">
+    <div class="wrapper ">
+
+        <div class="content">
+            <div class="container">
+                <?php include('./new_menus.php');
+                newMenus();
+                ?>
+                <br/>
+                <div class="about__container">
+                    <p style="text-align:center">
+                        The Guidance, Counseling, and Career Office (GCCSO) envisions to provide opportunities for students' personal integration, academic excellence & professional advancement
+                        The GCCSO dedicates itself to the holistic development of the student by;</p><br />
+                    <ul style="text-align:center">
+                        <li> 1. Assisting them in strengthening their awareness of oneself and others;</li>
+                        <li> 2. Providing support that would help them explore their full potential and make use of these to achieve their life goals;</li>
+                        <li> 3. Helping them develop the ability to cope with problem situation and make intelligent choices and decisions; and</li>
+                        <li> 4. Facilitating a climate for physical, intellectual, emotional, social, moral and spiritual growth.</li>
+                    </ul>
+
+                    <h3 style="text-align:center">GUIDANCE SERVICES</h3>
+                    <p style="text-align:center">Information, Individual Inventories, Psychological Testing, Counseling, Group Guidance, Follow-up, Referrals, Linkages, Evaluation, Research and. Community Outreach</p>
+                    <h3 style="text-align:center">CAREER DEVELOPMENT PROGRAMS</h3>
+                    <p style="text-align:center">Career Orientation, Career Planning, Career Talks, Mock Interview and Job Fairs</p>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3>ENRICHMENT PROGRAMS</h3>
+                            <ul>
+                                <li> SACOP - Student Academic Competence Program</li>
+                                <li>SEMAP - Self-Enhancement and Management Program</li>
+                                <li> HEART - Healthy Sexuality and Relationship Empowerment</li>
+                                <li> UECPPF - University of the East Caloocan Perks Peer Facilitators Training Program</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>SPECIAL GUIDANCE PROGRAMS</h3>
+                            <ul>
+                                <li> Adolescents from OFW Households Transformational Program</li>
+                                <li>Goal-Directed Intervention Program for Students-at-Risk</li>
+                                <li> Self-identified Lesbian, Gay, Bisexual and Transgender (LGBT) Students Support Program</li>
+                                <li> International Students Fellowship and Integration Program</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <h3 style="text-align:center">GUIDANCE PERSONNEL</h3><br/>
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <p>Mr. Antonio C. Postrado, Jr., LPT, RGC<br />
+                                <strong>Guidance Coordinator</strong>
+                            </p>
+                            <br />
+                        </div>
+                        <div class="col-lg-4">
+                            <p>Ms. Ma. Janine C. Bautista, LPT, RPm, RGC</p>
+                            <p>Mr. Jayson S. Parena, MS, RGC <br />
+                                <strong>Guidance Counselors</strong>
+                            </p>
+                        </div>
+                        <div class="col-lg-4">
+                            <p>Mr. Arnold A. de Silva, MA, RPm
+                                <strong>Psychometrician</strong><br />
+                                Ms. Jovine Dimple L. Dela Cruz, RPm
+                                <strong>Guidance Clerk</strong>
+                            </p>
+                        </div>
+                    </div>
+                    <br />
+                    <div class="row">
+                        <div class="col-lg-4">
+                            <ul>
+                                <li>Get in touch with the proactive and dynamic GCCSO Personnel at the</li>
+                                <li>Ground Floor, Old Academic Building and 2nd Floor, Dr. Lucio C. Tan Building</li>
+
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul>
+                                <li> University of the East 105 Samson Road, Caloocan City</li>
+                                <li>Monday-Friday 7:30 a.m. - 5:00 p.m.</li>
+                            </ul>
+                        </div>
+                        <div class="col-lg-4">
+                            <ul>
+                                <li> Email us: gco.cal@ue.edu.ph</li>
+                                <li>Call us: 8367-4572 loc. 175 (college) & 207 (Basic Education Department) / 8365-4124</li>
+                            </ul>
+                        </div>
+
+                    </div>
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h3 style="text-align:center">UE core values</h3> <br />
+                            <p style="text-align:center">The University adheres to the core values of Excellence, Integrity, Professionalism, Teamwork, Commitment, Transparency, Accountability and Social Responsibility</p>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <h3>~EHSD MISSION~</h3> <br />
+                            <p>We strive to be world-class grade school and high schools. We are student centered, multicultural and inter-racial schools that pursue excellence in Science, Technology, Mathematics, Languages, Music, Performing and Visual Arts, and Sports. We uphold sound socio-cultural, moral and ethical values. We aim to develop individuals who will become well-rounded individuals and worthy members of the society and the global community.</p>
+
+                        </div>
+                        <div class="col-lg-6">
+                            <h3>~EHSD GOALS~</h3> <br />
+                            <p>The Elementary and High School Department aims to become a vital instrument in the development of the students’ optimum potential, ideal values and characteristics and sense of nationalism, which are needed in the provision of the quality but affordable basic education.</p>
+                        </div>
+
+                    </div>
+
+                    <br /><br /><br />
+
+
+
+                </div>
+
+
+            </div>
+        </div>
+        <?php include('./footer.php'); ?>
